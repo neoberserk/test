42a379b38d777abb023a016f71d90b82fb321907
neoberserk <neoberserk1@gmail.com>
2017-02-10 22:13:58 +0700

    Add files via upload

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = 'hGBwB4/wbhe19IJlnatFrJ9ERhUDRvFBXOyWsA7KRkCTSEFViXBmDRGaQokKR03XDCQnH9fYHNDdAq4fGfWMeQ3zpUSDGEWWYthfIBTna1ZLDnEIXtnFZ5dOv6fW39zRfAOopxgITDlPjms7RA2vxQdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
