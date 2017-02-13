048c2f9e77c700dea5fd3c1331df2b6d2e0c4429
neoberserk <neoberserk1@gmail.com>
2017-02-13 13:49:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,7 +20,8 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+	//			'text' => $text
+				'text' => 'ทดสอบ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
